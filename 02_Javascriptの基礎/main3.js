d429c6960cef055cc6a86fdafdf59ab8ac5b42de
xiaoanise <61268911+xiaoanise@users.noreply.github.com>
2022-04-23 22:54:11 +0900

    commit

---
+++ 02_Javascriptの基礎/main3.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function keyUp(e) {
+  // console.log('keyUp');
+  // console.log(inputText.value);
+  // console.log(inputText.value.length);
+  // 入力された値を取得
+  let str = inputText.value;
+  // 残りの文字数
+  let num = 10 - str.length;
+  console.log(num);
+  // 文字数表示部分の要素を取得
+  const characterCount = document.getElementById('characterCount');
+  // 残りの文字数をセットして表示
+  characterCount.textContent = num;
+}
+
+const inputText = document.getElementById('inputText');
+inputText.addEventListener('keyup', keyUp, false);
