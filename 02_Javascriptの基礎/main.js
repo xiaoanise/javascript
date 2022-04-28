bb1a50fb9dc400bab9d3e96ad6bfc6ad619967b9
xiaoanise <61268911+xiaoanise@users.noreply.github.com>
2022-04-28 16:27:38 +0900

    commit

--- 02_Javascriptの基礎/main.js
+++ 02_Javascriptの基礎/main.js
@@ -1,7 +1,6 @@
 'use strict';
 
-let num ='1122234412222'
-
-let result = num.replace(/(\d)\1*/g), (content, $1) => {
-  return `${content.length}${content[0]}`
+if (true) {
+  var x = 5;
 }
+console.log(x);
