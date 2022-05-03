6ed1414d6b0ae667ebf80af6ca62de996e630ff7
xiaoanise <61268911+xiaoanise@users.noreply.github.com>
2022-05-03 22:07:04 +0900

    commit

---
+++ 05_event/05-05_dom-content-loaded.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// 例題
+// 「Now Loading...」表示 + 画像表示をする
+// DOMContentLoadedイベントが発生したら、alertダイアログを表示する。
+// loadイベントが発生したら、「Now Loading...」を非表示にする
+
+// DOMContentLoaded
+document.addEventListener('DOMContentLoaded', function() {
+  alert('DOMContentLoaded!');
+}, false);
+
+// load
+window.onload = function() {
+  const nowLoading = document.getElementById('nowLoading');
+  nowLoading.style.display = 'none';
+}
