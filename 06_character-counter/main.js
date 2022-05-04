9732516e69ec078f2bd7f3a6fffedff8e0cb2ffb
xiaoanise <61268911+xiaoanise@users.noreply.github.com>
2022-05-04 23:59:39 +0900

    commit

--- 06_character-counter/main.js
+++ 06_character-counter/main.js
@@ -1,15 +1,32 @@
 'use strict';
 
-const inputText = document.getElementById('inputText');
-inputText.addEventListener('keyup', function () {
-  console.log('keyUp');
-  console.log(inputText.value);
-  console.log(inputText.value.length); 
-  
+// イベントハンドラ
+function keyUp(e) {
+  // console.log('keyUp');
+  // console.log(inputText.value);
+  // console.log(inputText.value.length);
+  // 入力された値を取得
   let str = inputText.value;
+  str = str.replace(/\r?\n/g, '');
+  
+  // 残りの文字数
   let num = 10 - str.length;
   console.log(num);
-  
+  // 文字数表示部分の要素を取得
   const characterCount = document.getElementById('characterCount');
+  // 残りの文字数をセットして表示
   characterCount.textContent = num;
-}, false);
+
+  // 残り文字数表示部分のp要素を取得
+  const characterCountWrap = document.getElementById('characterCountWrap');
+  // 文字色の設定
+  if(num >= 0) {
+    characterCountWrap.style.color = 'black';
+  } else {
+    characterCountWrap.style.color = 'red';
+  }
+}
+
+// イベントハンドラの登録
+const inputText = document.getElementById('inputText');
+inputText.addEventListener('keyup', keyUp, false);
